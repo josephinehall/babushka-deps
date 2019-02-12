3bf0ec7178c7de062ceb8837f05b2bef10b442ba
Josephine Hall <josephinehall@gmail.com>
2019-02-12 14:56:20 +1300

    Restructure reps and add Caffeine

--- apps.rb
+++ apps.rb
@@ -1,14 +1,26 @@
-def cask(name); dep name, :template => "icelab:cask"; end # Avoid repitition below
+dep "apps" do
+  requires "mac app store apps"
+  requires "homebrew cask apps"
+end
 
-cask "dash"
-cask "fork"
-cask "google-chrome"
-cask "imageoptim"
-cask "iterm2"
-cask "launchbar"
-cask "licecap"
-cask "postico"
-cask "sublime-text"
-cask "virtualbox"
-cask "visual-studio-code"
-cask "vlc"
+dep "mac app store apps" do
+  requires "Pixelmator.mas"
+  requires "Ulysses.mas"
+end
+
+dep "homebrew cask apps" do
+  requires "caffeine"
+  requires "dash"
+  requires "fork"
+  requires "google-chrome"
+  requires "imageoptim"
+  requires "iterm2"
+  requires "launchbar"
+  requires "licecap"
+  requires "postico"
+  requires "sublime-text"
+  requires "virtualbox"
+  requires "visual-studio-code"
+  requires "vlc"
+end
+
